5ec1c9dadd1996e3edd9a65fb4741a41c629c41b
Riho yokoi <mjrwrbbc@gmail.com>
2021-11-28 17:22:43 +0900

    originの追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://127.0.0.1:8080/'
+        origins 'https://ik1-341-30511.vs.sakura.ne.jp/','http://127.0.0.1:8080/'
 
         resource '*',
         headers: :any,
