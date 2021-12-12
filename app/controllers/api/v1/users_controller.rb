9fee737c2e3d57df6bb117cb108bb0c5be1080e1
Riho yokoi <mjrwrbbc@gmail.com>
2021-12-12 11:03:44 +0900

    - update user_controller - edit production.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,9 +11,12 @@ module Api
             # Mypage（現在の登録家事件数／おすすめ担当日時）
             def about
                 user = User.find_by(id: current_user.id)
+                count = Chore.where(user_id: user.id).count
+                assignment_chore_day = AssignmentChoreDay.where(user_id: user.id)
                 # render json: user, each_serializer: UserSerializer, include: [ :assignment_chore_days ]
-                render json: user, each_serializer: UserSerializer, include: [ :assignment_chore_day ]
+                # render json: user, each_serializer: UserSerializer
 
+                render json: {user: user, count: count, assignment_chore_day: assignment_chore_day}
             end
             
         end
