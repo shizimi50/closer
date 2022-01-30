4c4f26a93423726e5be5ee96397b1ad4c4c28ce0
Riho yokoi <mjrwrbbc@gmail.com>
2022-01-30 23:34:34 +0900

    update: survey更新機能追加。担当家事曜日の種別カラム追加。

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
       end
 
       resources :surveys, only: [:index, :create]
+      put 'surveys' => 'surveys#update'
 
       resources :chores do
         collection do
--- db/migrate/20220127143558_add_assignment_date_to_surveys.rb
+++
@@ -1,5 +0,0 @@
-class AddAssignmentDateToSurveys < ActiveRecord::Migration[6.1]
-  def change
-    add_column :surveys, :assignment_date, :string
-  end
-end
